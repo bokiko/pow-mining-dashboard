f425039b52043cb0a229382cf2ca3581de60a81f
bokiko <124489102+bokiko@users.noreply.github.com>
2025-01-18 11:25:24 +0300

    Update index.tsx

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,4 +1,5 @@
 'use client'
+// ☝️ This directive is crucial - it marks this as a client component
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
